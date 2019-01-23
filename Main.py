62af56ce9c7ad316680f9c50f59d0e885bc70aff
Joan Andrés <andres6936@live.com>
2019-01-23 12:03:58 -0500

    Add Main

---
+++ Main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Joan Andrés
+# @Date: -*- -*- -*-
+# @Email: andres6936@gmail.com
+# @Email: andres6936@live.com
+
+from interfaz.InterfazElecciones import InterfazElecciones
+
+if __name__ == '__main__':
+    
+    interfaz = InterfazElecciones()
+    interfaz.setVisible(1)
+    
